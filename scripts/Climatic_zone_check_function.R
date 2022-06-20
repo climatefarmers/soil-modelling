46a7095ebc7bf01cb72278264a8c348f52802b34
JeremieO <jeremie@climatefarmers.org>
2022-06-20 16:48:18 +0200

    I suggest to get Climate_zone folder location path from climatic_zone_loc so that it is not hard-coded. Then, we can directly pull the map within the function and have only in argumets, a folder path, longitude and latitude.

--- scripts/Climatic_zone_check_function.R
+++ scripts/Climatic_zone_check_function.R
@@ -8,17 +8,17 @@
 # Read input data ----
 library(sf)
 
-clim.zone <- st_read("G:/Shared drives/Climate Farmers/07_Tech/Modelling/Climatic_zone/EnSv8/ens_v8.shp", stringsAsFactors=FALSE)
-enz.name<-c("Alpine north", "Boreal", "Nemoral", "Atlantic north", "Alpine south", "Continental", "Atlantic central", "Pannonian", "Lusitanian", "Anatolian", "Mediterranean mountains", "Mediterranean north", "Mediterranean south")
-
-# Function to extract the Environmental zone at a given coordinate ----
-# input has to be a map, and a table with coordinates, with column names x and y. 
-
-clime.zone.check<-function(clim.zone.map, coord, enz.name){
+clime.zone.check<-function(climatic_zone_loc, lon_farmer, lat_farmer){
+  # Function to extract the Environmental zone at a given coordinate ----
+  # Inputs are the local modelling-data repo folder path and longitude and latitude of the location considered. 
+  clim.zone.map <- st_read(file.path(climatic_zone_loc,"EnSv8/ens_v8.shp"), stringsAsFactors=FALSE)
+  enz.name<-c("Alpine north", "Boreal", "Nemoral", "Atlantic north", "Alpine south", "Continental", "Atlantic central", 
+              "Pannonian", "Lusitanian", "Anatolian", "Mediterranean mountains", "Mediterranean north", "Mediterranean south")
   # The map requires transformations to prevent error messages. 
   clim.zone_ll <- sf::st_transform(clim.zone.map, crs = 3857) 
   clim.zone_ll <- sf::st_transform(clim.zone_ll, crs = 4326) 
   # points are transformed to the same projection
+  coord <- data.frame(x=lon_farmer, y=lat_farmer)
   coords_t1 <- st_as_sf(coord, coords = c("x", "y"), crs= 4326)
   
   # st assumes planar, so one last transformation to planar
@@ -29,11 +29,6 @@ clime.zone.check<-function(clim.zone.map, coord, enz.name){
   enz.name[EnZ.class] 
   
 }
-
-
-# Run function ----
-coords <- data.frame(x=-7.332416805518278, y=39.822072494111474)
-clime.zone.check(clim.zone, coords)
 
 
 # Function to Overlap env zone with WoSIS ----
