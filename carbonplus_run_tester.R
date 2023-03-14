22324de0ffbe7b81aa965e683371cb944c9ecfa7
Fernando Moyano <fernando.moyano@mailbox.org>
2023-03-14 18:09:33 +0100

    settings

--- carbonplus_run_tester.R
+++ carbonplus_run_tester.R
@@ -8,14 +8,16 @@ rm(list=ls())
 library(tidyverse)
 library(jsonlite)
 
-debug_mode = TRUE  # Skip some steps. For now just skip fetching and       use dummy climate data.
+debug_mode = FALSE  # Skip some steps. For now just skip fetching and       use dummy climate data.
 
 source("carbonplus_trigger_Rscript.R")
 
 sensitive_data_loc <- "../sensitive-data"
 
 farmIds <- read_csv(file.path(sensitive_data_loc,"farmIds.csv"), show_col_types = FALSE)
-farmId <- farmIds$farmId[farmIds$farmer_name == "Herberto Brunk"]
+
+# farmId <- farmIds$farmId[farmIds$farmer_name == "Joao Rodrigues"]
+farmId <- farmIds$farmId[farmIds$farmer_name == "Manuel Die"]
 
 init_file <- fromJSON(file.path(sensitive_data_loc,"init_file.json"))
 
